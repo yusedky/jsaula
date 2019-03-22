91be385035856243a713c5a4bea175ef75178a6c
Irianne <yusedky@gmail.com>
2019-03-22 04:23:14 +0000

    transferencia de toda a parte do html para dentro do js juntamente com a correcao de bugs

--- Trabalhos/trabalho1/trab.js
+++ Trabalhos/trabalho1/trab.js
@@ -1,6 +1,92 @@
 var rolagem = ['d4', 'd6', 'd8', 'd10', 'd12'];
 
 
+
+// GERANDO BODY
+window.onload = function(){
+	
+	
+	
+// CABEÇALHO	
+	var body = document.getElementsByTagName("BODY")[0];
+	var titulo = document.createElement("h1");
+	var p1 = document.createElement("p");
+	var p2 = document.createElement("p");
+	var input = document.createElement("input");
+	input.id = "qtd"; 
+	var p3 = document.createElement("p");
+	
+	titulo.innerHTML = "Gerador de Fichas";
+	p1.innerHTML = "Gere automáticamente fichas de NPCs para sua mesa, escolha a baixo o tipo:";
+	p2.innerHTML = "Insira a quantidade a ser gerada de NPCs do mesmo tipo: ";
+	
+	body.appendChild(titulo);
+	body.appendChild(p1);
+	body.appendChild(p2);
+	p2.appendChild(input);
+
+
+	
+// CONTAINER DE BOTÕES	
+	p3.classList.add("container");
+	var bnt1 = document.createElement("button");
+	var bnt2 = document.createElement("button");
+	var bnt3 = document.createElement("button");
+	var bnt4 = document.createElement("button");
+	bnt4.classList.add("limpar");
+	
+	bnt1.innerHTML = "Comum"; 
+	bnt1.setAttribute("onclick" , "inicCommon()");
+	bnt2.innerHTML = "Inimigo"; 	
+	bnt2.setAttribute("onclick" , "inicEnemy()");
+	bnt3.innerHTML = "Aliado"; 
+	bnt3.setAttribute("onclick" , "inicAlly()");
+	bnt4.innerHTML = "Limpar"; 
+	bnt4.setAttribute("onclick" , "limpar()");
+	
+	body.appendChild(p3);
+	p3.appendChild(bnt1);
+	p3.appendChild(bnt2);
+	p3.appendChild(bnt3);
+	p3.appendChild(bnt4);
+	
+	
+	
+// TOPO DA TABELA	
+	var div = document.createElement("div");
+	div.id = "ficha"; 
+	var table = document.createElement("table");
+	var thead = document.createElement("thead");
+	var trowh = document.createElement("tr");
+	var thtipo = document.createElement("th");
+	var thpv = document.createElement("th");
+	var thpm = document.createElement("th");
+	var thconst = document.createElement("th");
+	var thdano = document.createElement("th");
+	
+	thtipo.innerHTML = "TIPO";
+	thpv.innerHTML = "PV";
+	thpm.innerHTML = "PM";
+	thconst.innerHTML = "CONSTITUIÇÃO";
+	thdano.innerHTML = "DANO";
+	var tbody = document.createElement("tbody");
+	tbody.id = "topicos";
+	
+	body.appendChild(div);
+	div.appendChild(table);
+	table.appendChild(thead);
+	thead.appendChild(trowh);
+	thead.appendChild(thtipo);
+	thead.appendChild(thpv);
+	thead.appendChild(thpm);
+	thead.appendChild(thconst);
+	thead.appendChild(thdano);
+	table.appendChild(tbody);
+}
+
+
+
+// TABELA
 function montarTabela(resp){
 	var qtd = document.getElementById("qtd").value;
 
@@ -44,6 +130,7 @@ function montarTabela(resp){
 
 
 
+// BOTÕES
 function inicCommon(){
   	montarTabela({dados: [{tipo:"Comum", pv: "", pm:"", const:"", dano:""}]});
 }
@@ -55,9 +142,6 @@ function inicEnemy(){
 function inicAlly(){
 	montarTabela({dados: [{tipo:"Aliado", pv: "", pm:"", const:"", dano:""}]});
 }
-
-
-
 
 function limpar(){
 	var table = document.getElementById("topicos");
