6d0fcdbb12ca4a241b6f5688feb306eb2f104d16
Irianne <yusedky@gmail.com>
2019-03-22 02:02:13 +0000

    opcao de escolher o numero de npcs gerados

--- Trabalhos/trabalho1/trab.js
+++ Trabalhos/trabalho1/trab.js
@@ -6,28 +6,44 @@ function montarTabela(resp){
 	ficha.style.display= "block";
 
 	
+	var qtd = document.getElementById("qtd").value;
 	var table = document.getElementById("topicos");
-	var vetObj = resp.dados;
-	var tr = document.createElement("tr");
-	for (var i=0; i < vetObj.length; i++) {
-		var tdTipo = document.createElement("td");
-		var tdPV = document.createElement("td");
-		var tdPM = document.createElement("td");
-		var tdConst = document.createElement("td");
-		var tdDano = document.createElement("td");
-		tdTipo.innerHTML = vetObj[i].tipo;
-		tdPV.innerHTML = Math.floor(Math.random() * 40) + 10;
-		tdPM.innerHTML = Math.floor(Math.random() * 30);
-		tdConst.innerHTML = Math.floor(Math.random() * 15) + 5;
-		tdDano.innerHTML = (Math.floor(Math.random() * 3) + 1) + rolagem[Math.floor(Math.random() * 5)];
-		tr.appendChild(tdTipo);
-		tr.appendChild(tdPV);
-		tr.appendChild(tdPM);
-		tr.appendChild(tdConst);
-		tr.appendChild(tdDano);
+
+	
+
+	for (var j=0; j < qtd; j++ ) {
+		
+		var vetObj = resp.dados;
+		var tr = document.createElement("tr");
+		for (var i=0; i < vetObj.length; i++) {
+			var tdTipo = document.createElement("td");
+			var tdPV = document.createElement("td");
+			var tdPM = document.createElement("td");
+			var tdConst = document.createElement("td");
+			var tdDano = document.createElement("td");
+			tdTipo.innerHTML = vetObj[i].tipo;
+			tdPV.innerHTML = Math.floor(Math.random() * 40) + 10;
+			tdPM.innerHTML = Math.floor(Math.random() * 30);
+			tdConst.innerHTML = Math.floor(Math.random() * 15) + 5;
+			tdDano.innerHTML = (Math.floor(Math.random() * 3) + 1) + rolagem[Math.floor(Math.random() * 5)];
+			tr.appendChild(tdTipo);
+			tr.appendChild(tdPV);
+			tr.appendChild(tdPM);
+			tr.appendChild(tdConst);
+			tr.appendChild(tdDano);
+		}
+		table.appendChild(tr);
+		var div = document.querySelector("#topicos");
+		
+	
 	}
-	table.appendChild(tr);
-	var div = document.querySelector("#topicos");
+	
+	
+	
+	
+	
+	
+
 }
 
 
